3cdfbf5c71ef2e8a8823cc66f941cc5c93c20753
zormax <okocadere@gmail.com>
2022-10-19 10:56:39 +0300

    multi-satege docker file added

--- visits/index.js
+++ visits/index.js
@@ -10,13 +10,13 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
-  process.exit(0);
+  //process.exit(0);
   client.get('visits', (err, visits) => {
-    res.send('Number of visits ' + visits);
+    res.send('Number of visits omer' + visits);
     client.set('visits', parseInt(visits) + 1);
   });
 });
 
 app.listen(8081, () => {
-  console.log('listening on port 8081');
+  console.log('listening on port 4001');
 });
